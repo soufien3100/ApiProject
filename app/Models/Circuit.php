981977cc81fe5c24b0ed8da3b3846613e21f1af8
Levi <M.Jason.45@hotmail.fr>
2021-09-16 09:50:31 +0200

    update and create only some variable

--- app/Models/Circuit.php
+++ app/Models/Circuit.php
@@ -14,6 +14,27 @@ class Circuit extends Model
 
         $this->circuitRef = $data['circuitRef'];
         $this->name = $data['name'];
+        
+        if (isset($data['location'])) {
+            $this->location = $data['location'];
+        }
+
+        if (isset($data['country'])) {
+            $this->location = $data['country'];
+        }
+
+        if (isset($data['lat'])) {
+            $this->location = $data['lat'];
+        }
+
+        if (isset($data['lng'])) {
+            $this->location = $data['lng'];
+        }
+
+        if (isset($data['alt'])) {
+            $this->location = $data['alt'];
+        }
+
         $this->url = $data['url'];
 
         $this->save();
@@ -21,7 +42,47 @@ class Circuit extends Model
 
     public function updateCircuit($data) {
         
-        $this->circuitRef = $data['circuitRef'];
+        if (isset($data['circuitRef'])) {
+            $this->circuitRef = $data['circuitRef'];
+        }
+
+        if (isset($data['name'])) {
+            $this->name = $data['name'];
+        }
+
+        if (isset($data['location'])) {
+            $this->location = $data['location'];
+        } else if (is_null($data['location'])) {
+            $this->location = $data['location'];
+        }
+
+        if (isset($data['country'])) {
+            $this->country = $data['country'];
+        } else if (is_null($data['country'])) {
+            $this->location = $data['country'];
+        }
+
+        if (isset($data['lat'])) {
+            $this->lat = $data['lat'];
+        } else if (is_null($data['lat'])) {
+            $this->location = $data['lat'];
+        }
+
+        if (isset($data['lng'])) {
+            $this->lng = $data['lng'];
+        } else if (is_null($data['lng'])) {
+            $this->location = $data['lng'];
+        }
+
+        if (isset($data['alt'])) {
+            $this->alt = $data['alt'];
+        } else if (is_null($data['alt'])) {
+            $this->location = $data['alt'];
+        }
+
+        if (isset($data['url'])) {
+            $this->url = $data['url'];
+        }
 
         $this->save();
     }
