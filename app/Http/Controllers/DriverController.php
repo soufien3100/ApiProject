a22432584092ea57df26ac60c1966fa1c9aba180
Levi <M.Jason.45@hotmail.fr>
2021-09-16 11:24:33 +0200

    add functionnal crud for driver

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -11,61 +11,26 @@ use App\Http\Resources\DriverResource;
 
 class DriverController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-
     public function index()
     {
         return Response(Driver::all());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-
     public function store(Request $request)
     { 
-        //dd($request->input('surname'));
-        $driver = new Driver() ; //creer une nouvelle instance 
-        $driver->createDriver($request->all()); // la fontion est le model 
+        $driver = new Driver();
+        $driver->createDriver($request->all()); 
 
-        return response()->json($driver,201); //retourner un format json  //status 201 
+        return response()->json($driver,201);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Driver  $driver
-     * @return \Illuminate\Http\Response
-     * 
-     */
-    public function show($id) //Driver non typer un model 
+    public function show($id)
     {
+        $driver = Driver::find($id);
 
-         $driver = Driver::find($id);
-            if($driver) {
-                return new DriverResource($driver);
-               // return  Response($driver);
-            }
-            return response()->json('Driver not found',404); 
-            
-        //ou $driver = new Driver()
-        //$driver->findOrFail('toto'));
+        return response($driver, 200);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Driver  $driver
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Driver $driver)
     {
         $driver->updateDriver($request->all());
@@ -73,12 +38,6 @@ class DriverController extends Controller
         return response()->json($driver,200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Driver  $driver
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Driver $driver)
     {
         $driver->delete();
--- app/Models/Driver.php
+++ app/Models/Driver.php
@@ -8,27 +8,74 @@ use Illuminate\Database\Eloquent\Model;
 class Driver extends Model
 {
     use HasFactory;
-    protected $primaryKey = 'driverId' ; 
+    protected $primaryKey = 'driverId';
 
-    protected $hidden = ['created_at','update_at'] ; 
+    protected $hidden = ['created_at','update_at'];
 
     public function createDriver($data) {
 
         $driver =new Driver();
 
-        $this->driverRef = $data['driverRef'] ; 
-        $this->forename = $data['forename'] ; 
+        $this->driverRef = $data['driverRef'];
+
+        if (isset($data['number'])) {
+            $this->number = $data['number'];
+        }
+
+        if (isset($data['code'])) {
+            $this->number = $data['code'];
+        }
+
+        $this->forename = $data['forename'];
         $this->surname = $data['surname'];
+
+        if (isset($data['dob'])) {
+            $this->number = $data['dob'];
+        }
+
+        if (isset($data['nationality'])) {
+            $this->number = $data['nationality'];
+        }
+
         $this->url = $data['url'];
        
         $this->save();
     }
     
     public function updateDriver($data) {
-        $this->driverRef = $data['driverRef'];
-        $this->surname = $data['surname'];
+
+        if (isset($data['driverRef'])) {
+            $this->driverRef = $data['driverRef'];
+        }
+
+        if (array_key_exists('number', $data)) {
+            $this->number = $data['number'];
+        }
+
+        if (array_key_exists('code', $data)) {
+            $this->code = $data['code'];
+        }
+
+        if (isset($data['forename'])) {
+            $this->forename = $data['forname'];
+        }
        
-       
+        if (isset($data['surname'])) {
+            $this->surname = $data['surname'];
+        }
+
+        if (array_key_exists('dob', $data)) {
+            $this->dob = $data['dob'];
+        }
+
+        if (array_key_exists('nationality', $data)) {
+            $this->nationality = $data['nationality'];
+        }
+
+        if (isset($data['url'])) {
+            $this->url = $data['url'];
+        }
+
         $this->save(); 
     }
     
