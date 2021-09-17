f691f02130d7e0c357cb3e99698023f10674d6fe
Soufien <soufien.araissia@yahoo.fr>
2021-09-17 10:02:33 +0200

    CRUD SHOW ID

--- app/Http/Controllers/RaceController.php
+++ app/Http/Controllers/RaceController.php
@@ -10,32 +10,16 @@ use App\Http\Resources\RaceResource;
 
 class RaceController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         return Response(Race::all());
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $race = new Race() ; //creer une nouvelle instance 
@@ -43,12 +27,6 @@ class RaceController extends Controller
         return response()->json($race,201);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Race  $race
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         $race = Race::find($id);
@@ -59,34 +37,12 @@ class RaceController extends Controller
         return response()->json('Racer not found',404); 
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Race  $race
-     * @return \Illuminate\Http\Response
-     */
-    
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Race  $race
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Race $race)
     {
-        $race->updateDriver($request->all());
-
+        $race->updateRace($request->all());
         return response()->json($race,200);
     }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Race  $race
-     * @return \Illuminate\Http\Response
-     */
+    
     public function destroy(Race $race)
     {
         $race->delete();
